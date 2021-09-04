13f5cf1f7deff32a7eae31db38cd6cfcc208b4a0
nil <nscode@sina.com>
2021-09-04 22:47:19 +0800

    加载 image

--- Webpack/webpack.config.js
+++ Webpack/webpack.config.js
@@ -5,14 +5,40 @@ module.exports = {
     entry:'./scr/index.js',
     output:{
         path:path.resolve(__dirname,'dist'),
-        filename:'bundle.js'
+        filename:'bundle.js',
+        publicPath:'dist/'
     },
     module:{
         rules:[
             {
                 test:/\.css$/,
                 use:["style-loader","css-loader"]
-            }
+            },
+            {
+                test:/\.(jpg|png|gif|jpeg)$/,
+                use:[
+                    {
+                        loader:'url-loader',
+                        options:{
+                            // 1.小于13000 时用base64 2.大于yong file loader
+                            limit:88848
+                        }
+                    }
+                ]
+            },
+            {
+                test:/\.(jpg|png|gif|jpeg)$/,
+                use:[
+                    {
+                        loader:'file-loader',
+                        // options:{
+                        //     // 1.小于13000 时用base64 2.大于yong file loader
+                        //     limit:88848
+                        // }
+                    }
+                ]
+            },
+            
         ]
     }
 }
